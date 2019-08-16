32d4b373a90f8de4a694a0772fbd0c06b2107848
Neal Richardson <neal.p.richardson@gmail.com>
2019-08-16 08:45:21 -0700

    Add curl script

---
+++ bintray-win-upload.sh
@@ -0,0 +1,6 @@
+#! /bin/bash
+upload_file() {
+  curl -fsSv -u "${BINTRAY_USER}:${BINTRAY_APIKEY}" -X PUT "https://api.bintray.com/content/nealrichardson/arrow-r/arrow/latest/bin/windows/contrib/3.6/$1?override=1&publish=1" --data-ascii "@$1"
+}
+
+ls "$1" | xargs -n 1 upload_file
