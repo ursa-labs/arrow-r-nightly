3aaedb318fe40d142a3b33fb7e0d79cc23a9a84d
Neal Richardson <neal.p.richardson@gmail.com>
2019-08-16 08:53:02 -0700

    Try this'

--- bintray-win-upload.sh
+++ bintray-win-upload.sh
@@ -2,5 +2,6 @@
 upload_file() {
   curl -fsSv -u "${BINTRAY_USER}:${BINTRAY_APIKEY}" -X PUT "https://api.bintray.com/content/nealrichardson/arrow-r/arrow/latest/bin/windows/contrib/3.6/$1?override=1&publish=1" --data-ascii "@$1"
 }
+export -f upload_file
 
-ls "$1" | xargs -n 1 upload_file
+ls "$1" | xargs -n 1 -I{} bash -c "upload_file {}"
