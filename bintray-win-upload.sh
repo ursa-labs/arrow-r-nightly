80366f39bd7b8381b2bdd7f6b7342b9692826fa5
Neal Richardson <neal.p.richardson@gmail.com>
2019-08-16 12:33:03 -0700

    Fix RVERSION lookup scope

--- bintray-win-upload.sh
+++ bintray-win-upload.sh
@@ -1,8 +1,7 @@
 #! /bin/bash
 
-RVERSION=$(Rscript -e 'rv <- R.version; cat(paste0(rv$major, ".", unlist(strsplit(rv$minor, ".", fixed=TRUE))[1]))')
-
 upload_file() {
+  RVERSION=$(Rscript -e 'rv <- R.version; cat(paste0(rv$major, ".", unlist(strsplit(rv$minor, ".", fixed=TRUE))[1]))')
   curl -fsSv -u "${BINTRAY_USER}:${BINTRAY_APIKEY}" -X PUT "https://api.bintray.com/content/nealrichardson/arrow-r/arrow/latest/bin/windows/contrib/${RVERSION}/$1?override=1&publish=1" --data-binary "@$1"
 }
 export -f upload_file
