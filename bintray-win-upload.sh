08af37b9e8f153f533022a0f4bfcb1b2a969198d
Neal Richardson <neal.p.richardson@gmail.com>
2019-08-16 11:12:26 -0700

    Try it here

--- bintray-win-upload.sh
+++ bintray-win-upload.sh
@@ -1,7 +1,5 @@
 #! /bin/bash
 
-RVERSION=$(Rscript -e 'rv <- R.version; cat(paste0(rv$major, ".", unlist(strsplit(rv$minor, ".", fixed=TRUE))[1]))')
-
 upload_file() {
   curl -fsSv -u "${BINTRAY_USER}:${BINTRAY_APIKEY}" -X PUT "https://api.bintray.com/content/nealrichardson/arrow-r/arrow/latest/bin/windows/contrib/${RVERSION}/$1?override=1&publish=1" --data-binary "@$1"
 }
