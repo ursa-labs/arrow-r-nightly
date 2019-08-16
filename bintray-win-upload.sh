f81ab0a3ca02a4d2e504e76cad0c1927bf7323bd
Neal Richardson <neal.p.richardson@gmail.com>
2019-08-16 11:05:22 -0700

    Determine R version

--- bintray-win-upload.sh
+++ bintray-win-upload.sh
@@ -1,7 +1,10 @@
 #! /bin/bash
+
+RVERSION=$(Rscript -e 'rv <- R.version; cat(paste0(rv$major, ".", unlist(strsplit(rv$minor, ".", fixed=TRUE))[1]))')
+
 upload_file() {
-  curl -fsSv -u "${BINTRAY_USER}:${BINTRAY_APIKEY}" -X PUT "https://api.bintray.com/content/nealrichardson/arrow-r/arrow/latest/bin/windows/contrib/3.6/$1?override=1&publish=1" --data-binary "@$1"
+  curl -fsSv -u "${BINTRAY_USER}:${BINTRAY_APIKEY}" -X PUT "https://api.bintray.com/content/nealrichardson/arrow-r/arrow/latest/bin/windows/contrib/${RVERSION}/$1?override=1&publish=1" --data-binary "@$1"
 }
 export -f upload_file
 
-ls "$1" | xargs -n 1 bash -c "upload_file $@"
+ls $1 | xargs -n 1 bash -c "upload_file $@"
