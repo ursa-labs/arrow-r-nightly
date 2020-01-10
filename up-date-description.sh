60cf731f0a770a48b13d2e32cb2f5f4f3578463c
Neal Richardson <neal.p.richardson@gmail.com>
2020-01-09 16:44:02 -0800

    Linux source packaging (#1)
    Step 1 of supporting linux nightly + binary builds: nightly C++ and R source packages

--- up-date-description.sh
+++ up-date-description.sh
@@ -1,7 +1,7 @@
 #! /bin/bash
 
-TODAY=$(date +%Y%m%d)
+: ${DATE:="$(date +%Y%m%d)"}
 sed -i.bak -E -e \
-  's/(^Version: [0-9]+\.[0-9]+\.[0-9]+).*$/\1.'"${TODAY}"'/' \
+  's/(^Version: [0-9]+\.[0-9]+\.[0-9]+).*$/\1.'"${DATE}"'/' \
   DESCRIPTION
 rm -f DESCRIPTION.bak
