592fc02b351ab5cf0a94ebce3771b3308fc1d362
Neal Richardson <neal.p.richardson@gmail.com>
2020-01-09 19:47:42 -0800

    More verbosity

--- linux/install-arrow.R
+++ linux/install-arrow.R
@@ -1,8 +1,9 @@
 source("https://raw.githubusercontent.com/apache/arrow/3a0abf03c07473a84123e4805157348d24e80220/ci/etc/rprofile")
-install.packages("arrow", repos = c(
-  "https://dl.bintray.com/ursalabs/arrow-r",
-  getOption("repos")
-))
+install.packages(
+  "arrow",
+  repos = c("https://dl.bintray.com/ursalabs/arrow-r", getOption("repos")),
+  verbose = TRUE
+)
 
 library(arrow)
 read_parquet(system.file("v0.7.1.parquet", package = "arrow"))
