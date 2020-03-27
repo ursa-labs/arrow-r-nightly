855700d4c61fbf786ea67d289aba2e79d5392600
Neal Richardson <neal.p.richardson@gmail.com>
2020-03-27 16:07:19 -0700

    Only prune nightly builds

--- prune-old-packages.R
+++ prune-old-packages.R
@@ -7,13 +7,27 @@ MAX_REQUESTS <- as.numeric(Sys.getenv("MAX_REQUESTS", 30))
 auth <- authenticate("nealrichardson", Sys.getenv("BINTRAY_APIKEY"))
 all_files_url <- "https://api.bintray.com/packages/ursalabs/arrow-r/arrow/files"
 
+get_patch_version <- function(path) {
+  # For URLs containing arrow version x.y.z.p, return p, or 0 if version is x.y.z
+  v <- unlist(numeric_version(sub("^.*/arrow[-_]([0-9\\.]+)\\.[tz].*$", "\\1", path)))
+  if (length(v) > 3) {
+    return(v[4])
+  } else {
+    return(0L)
+  }
+}
+
 files <- all_files_url %>%
   GET(config = auth) %>%
   content("text") %>%
   jsonlite::fromJSON(simplifyDataFrame = TRUE) %>%
   mutate(date = as.Date(substr(created, 1, 10))) %>%
   select(path, size, date) %>%
+  # Only consider those older than MAX_AGE
   filter(date < (Sys.Date() - MAX_AGE)) %>%
+  # Only purge dev/nightly versions (those x.y.z.p versions with p > 1000)
+  filter(get_patch_version(path) > 1000) %>%
+  # Sort the biggest first
   arrange(desc(size)) %>%
   pull(path) %>%
   head(MAX_REQUESTS)
