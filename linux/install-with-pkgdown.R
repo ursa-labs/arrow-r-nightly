f5af0cf64af5d0e0579bf35c1e42aad468279ba7
Neal Richardson <neal.p.richardson@gmail.com>
2020-04-01 16:03:20 -0700

    Try downgrading pkgdown

--- linux/install-with-pkgdown.R
+++ linux/install-with-pkgdown.R
@@ -4,4 +4,6 @@ options(
   repos = c("https://dl.bintray.com/ursalabs/arrow-r", getOption("repos"))
 )
 install.packages("arrow", dependencies = TRUE, verbose = TRUE)
-install.packages("pkgdown", verbose = TRUE)
+# install.packages("pkgdown", verbose = TRUE)
+install.packages("remotes")
+remotes::install_github("r-lib/pkgdown@v1.4.1")
