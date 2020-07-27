1d1e5ea59d935b8f5e6a3da206e2633be3098cf0
Neal Richardson <neal.p.richardson@gmail.com>
2020-07-27 13:34:49 -0700

    Strip the shared library on macOS binary packaging

--- build-binary-and-upload.R
+++ build-binary-and-upload.R
@@ -1,3 +1,5 @@
+on_windows <- tolower(Sys.info()[["sysname"]]) == "windows"
+
 # Install dependencies by installing (yesterday's) binary, then removing it
 install.packages("arrow",
   type = "binary",
@@ -7,10 +9,15 @@ remove.packages("arrow")
 
 # Build
 Sys.setenv(MAKEFLAGS = paste0("-j", parallel::detectCores()))
+INSTALL_opts <- "--build"
+if (!on_windows) {
+  # Windows doesn't support the --strip arg
+  INSTALL_opts <- c(INSTALL_opts, "--strip")
+}
 install.packages("arrow",
   type = "source",
   repos = "https://dl.bintray.com/ursalabs/arrow-r",
-  INSTALL_opts="--build"
+  INSTALL_opts = INSTALL_opts
 )
 
 # Test
@@ -18,7 +25,6 @@ library(arrow)
 read_parquet(system.file("v0.7.1.parquet", package = "arrow"))
 
 # Upload
-on_windows <- tolower(Sys.info()[["sysname"]]) == "windows"
 tools::write_PACKAGES(".", type = ifelse(on_windows, "win.binary", "mac.binary"))
 
 Sys.setenv(REPO_PATH = contrib.url("", type = "binary"))
