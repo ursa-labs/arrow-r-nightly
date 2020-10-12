5da88e15e57b926b52eeaf56dc193334d320bb94
Neal Richardson <neal.p.richardson@gmail.com>
2020-10-12 15:28:53 -0700

    Move arrow installation for pkgdown

--- linux/install-with-pkgdown.R
+++ linux/install-with-pkgdown.R
@@ -4,4 +4,4 @@ options(
   repos = c("https://arrow-r-nightly.s3.amazonaws.com", getOption("repos"))
 )
 install.packages(c("remotes", "pkgdown"), verbose = TRUE)
-remotes::install_deps("arrow/r", dependencies = TRUE, verbose = TRUE)
+remotes::install_local("arrow/r", dependencies = TRUE, verbose = TRUE, build = FALSE)
