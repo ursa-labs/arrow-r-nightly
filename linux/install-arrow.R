fa2a83c2cee4d7452f81a285fe08fc7c075cd876
Neal Richardson <neal.p.richardson@gmail.com>
2020-10-21 15:48:15 -0700

    Try to map fedora to something useful

--- linux/install-arrow.R
+++ linux/install-arrow.R
@@ -4,6 +4,15 @@ source("https://raw.githubusercontent.com/apache/arrow/master/ci/etc/rprofile")
 install.packages("https://github.com/r-lib/remotes/archive/master.tar.gz", repos = NULL, type = "source")
 remotes::install_github("nealrichardson/distro")
 d <- distro::distro()
+if (!(d$id %in% names(remotes:::supported_os_versions()))) {
+  # Let's see if we can map this to something else we know
+  os <- paste0(d$id, "-", d$short_version)
+  lookup <- utils::read.csv("linux/distro-map.csv", stringsAsFactors = FALSE)
+  if (os %in% lookup$actual) {
+    new <- unlist(strsplit(lookup$use_this[lookup$actual == os], "-"))
+    d <- list(id = new[1], short_version = new[2])
+  }
+}
 # curl has same sysreqs so use it today (since arrow with updated sysreqs isn't yet on RSPM)
 sysreqs <- remotes::system_requirements(d$id, d$short_version, package = "curl")
 if (any(grepl("apt-get", sysreqs))) {
