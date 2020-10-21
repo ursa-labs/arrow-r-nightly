1618438f8af53aa72aab8ca35b579ac7568abe86
Neal Richardson <neal.p.richardson@gmail.com>
2020-10-21 12:16:17 -0700

    Try this to install system deps for testing binary packages

--- linux/install-arrow.R
+++ linux/install-arrow.R
@@ -1,4 +1,14 @@
 source("https://raw.githubusercontent.com/apache/arrow/master/ci/etc/rprofile")
+
+# We need dev remotes for system_requirements
+install.packages("pak")
+pak::pkg_install("r-lib/remotes")
+pak::pkg_install("nealrichardson/distro")
+d <- distro::distro()
+# curl has same sysreqs so use it today (since arrow with updated sysreqs isn't yet on RSPM)
+sysreqs <- remotes::system_requirements(d$id, d$short_version, package = "curl")
+for (s in sysreqs) system(s)
+
 install.packages(
   "arrow",
   repos = c("https://arrow-r-nightly.s3.amazonaws.com", getOption("repos")),
