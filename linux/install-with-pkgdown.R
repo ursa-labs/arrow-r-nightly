e903f8804922492e72d126562ec8f37ec5409261
Jonathan Keane <jkeane@gmail.com>
2021-02-23 12:35:29 -0600

    use development pkgdown

--- linux/install-with-pkgdown.R
+++ linux/install-with-pkgdown.R
@@ -3,5 +3,7 @@ options(
   install.packages.check.source = "no",
   repos = c("https://arrow-r-nightly.s3.amazonaws.com", getOption("repos"))
 )
-install.packages(c("remotes", "pkgdown"), verbose = TRUE)
+install.packages("remotes", verbose = TRUE)
+# when https://github.com/r-lib/pkgdown/pull/1466 is released, can go back to std install
+remotes::install_github("r-lib/pkgdown")
 remotes::install_local("arrow/r", dependencies = TRUE, verbose = TRUE, build = FALSE)
