db16d00c8ec3703266dbb1bdcdd1a1becf95be63
Jonathan Keane <jkeane@gmail.com>
2021-12-09 08:55:45 -0600

    Install patch on Linuxes for patching snappy

--- linux/install-system-requirements.sh
+++ linux/install-system-requirements.sh
@@ -10,3 +10,19 @@ else
   apt-get update
   apt-get install -y libcurl4-openssl-dev libssl-dev
 fi
+
+# Install patch if it doesn't already exist
+# Only needed while we are patching snappy
+ if [ ! $(command -v patch) ]; then
+   if [ "`which dnf`" ]; then
+     dnf install -y patch
+   elif [ "`which yum`" ]; then
+     yum install -y patch
+   elif [ "`which zypper`" ]; then
+     zypper install -y patch
+   else
+     apt-get update
+     apt-get install -y patch
+   fi
+ fi
+ 
